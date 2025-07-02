30e78edcd82dfd9f6b2a4debe011cb69b705f3a0
ozyurtyakup01 <ozyurt_yakup01@student.smc.edu>
2025-07-02 14:58:12 -0700

    added required functions

---
+++ math.js
@@ -0,0 +1,36 @@
+// Returns the square of a number
+function square(n) 
+{
+  return n * n;
+}
+
+// Returns true if the number is even
+function isEven(n) 
+{
+  return n % 2 === 0;
+}
+
+// Returns true if the number is odd
+function isOdd(n) 
+{
+  return n % 2 !== 0;
+}
+
+// Multiplies two numbers
+function multiply(left, right) 
+{
+  return left * right;
+}
+
+//
+function divide(left, right) 
+{
+  return ;
+}
+
+//
+function substract(left, right) 
+{
+  return ;
+}
+//
